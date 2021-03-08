0f78b166110b408e37f6e9fff75a5f088b10ca1f
Kabs <70097297+lodkabs@users.noreply.github.com>
2021-03-08 09:15:18 +0000

    Got something working for the stream, though not quite perfect at the moment

--- rules_scroll.py
+++ rules_scroll.py
@@ -1,4 +1,5 @@
 import obspython as obs
+import math
 import time
 
 def script_description():
@@ -9,27 +10,12 @@ def script_description():
 
 
 # User Data Setting:
-
-def populate_list_property_with_source_names(list_property):
-    sources = obs.obs_enum_sources()
-    obs.obs_property_list_clear(list_property)
-    obs.obs_property_list_add_string(list_property, "", "")
-    for source in sources:
-        name = obs.obs_source_get_name(source)
-        obs.obs_property_list_add_string(list_property, name, name)
-    obs.source_list_release(sources)
-
-def populate_list_property_with_filter_names(source, list_property):
-    filters = obs.obs_source_enum_filters(source)
-    obs.obs_property_list_clear(list_property)
-    obs.obs_property_list_add_string(list_property, "", "")
-    for filterItem in filters:
-        name = obs.obs_source_get_filter_by_name(source, filterItem)
-        obs.obs_property_list_add_string(list_property, name, name)
+pause_after = 0
+continue_after = 0
 
 def script_defaults(settings):
-    obs.obs_data_set_default_int(settings, "pause_after", 500)
-    obs.obs_data_set_default_int(settings, "continue_after", 500)
+    obs.obs_data_set_default_int(settings, "pause_after", 5)
+    obs.obs_data_set_default_int(settings, "continue_after", 5)
 
 
 # Called to display the properties GUI
@@ -48,4 +34,60 @@ def script_update(settings):
 #    restore_sceneitem_after_shake()
     pause_after = obs.obs_data_get_int(settings, "pause_after")
     continue_after = obs.obs_data_get_int(settings, "continue_after")
+
+
+source_name = "Rules"
+filter_name = "Scroll"
+currently_scrolling = False
+time_passed = 0
+
+def script_tick(seconds):
+    global source_name, filter_name
+    global currently_scrolling, time_passed
+
+    cond1 = currently_scrolling and time_passed >= pause_after
+    cond2 = not currently_scrolling and time_passed >= continue_after
+    if cond1 or cond2:
+
+        source_item = obs.obs_get_source_by_name(source_name)
+
+        if source_item:
+            filter_item = obs.obs_source_get_filter_by_name(source_item, filter_name)
+            filter_data = obs.obs_source_get_settings(filter_item)
+
+            if cond1:
+                obs.obs_data_set_int(filter_data, "speed_y", 1)
+                obs.obs_source_update(filter_item, filter_data)
+                currently_scrolling = False
+                time_passed = -1
+            elif cond2:
+                obs.obs_data_set_int(filter_data, "speed_y", 100)
+                obs.obs_source_update(filter_item, filter_data)
+                currently_scrolling = True
+                time_passed = -1
+
+            obs.obs_data_release(filter_data)
+            obs.obs_source_release(filter_item)
+
+        obs.obs_source_release(source_item)
+
+    time_passed += 1
+
+
+def script_load(settings):
+    global source_name, filter_name
+
+    source_item = obs.obs_get_source_by_name(source_name)
+
+    if source_item:
+        filter_item = obs.obs_source_get_filter_by_name(source_item, filter_name)
+        filter_data = obs.obs_source_get_settings(filter_item)
+
+        obs.obs_data_set_int(filter_data, "speed_y", 0)
+        obs.obs_source_update(filter_item, filter_data)
+
+        obs.obs_data_release(filter_data)
+        obs.obs_source_release(filter_item)
+
+    obs.obs_source_release(source_item)
 
