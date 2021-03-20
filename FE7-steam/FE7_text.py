dbbc6474eb46586b4ead79dd9e76dfc144efd354
Kabs <70097297+lodkabs@users.noreply.github.com>
2021-03-20 16:05:24 +0000

    Don't use exit(), it closes OBS entirely ^_^'

--- FE7-steam/FE7_text.py
+++ FE7-steam/FE7_text.py
@@ -1,10 +1,16 @@
 import obspython as obs
+import os
+import inspect
 
 
-# Global variables for rules text
-source_name         = "FE_GBA_rules"
-filter_name         = "Scroll"
-text_file           = "rule_set.txt"
+# Global variables for locale objects
+source_name    = "FE_GBA_rules"
+filter_name    = "Scroll"
+location       = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+text_file_name = "rule_set.txt"
+text_file      = f"{location}/{text_file_name}"
+
+# Global variables for rules source
 rules_source        = None
 rules_data          = None
 rules_filter_source = None
@@ -18,19 +24,20 @@ display_time  = 5000
 
 
 def script_description():
-    return f"Fire Emblem GBA-style text scroll
-             Create a text source called \"{source_name}\", with a
-             scroll filter called \"{filter_name}\n.
-             Add rule set to local file called \"{text_file}\", in the same
-             location as this script"
+    return f"""Fire Emblem GBA-style text scroll
+               Create a text source called \"{source_name}\",
+               with a scroll filter called \"{filter_name}\".
+               Add rule set to local file called \"{text_file_name}\",
+               in the same location as this script"""
 
 
-def reset_rules_variables:
+def reset_rules_variables():
     global source_name, filter_name, text_file
     global rules_source, rules_data, rules_list
     global rules_filter_source, rules_filter_data
     global next_line, time_per_char, display_time
 
+    ok = True
     obs.timer_remove(rules_scrolling)
 
     # Reset text
@@ -40,26 +47,27 @@ def reset_rules_variables:
 
     # Get rule set from local file
     with open(text_file) as f:
-        rules_list = f.read().splitline()
+        rules_list = f.read().splitlines()
 
     # Get source
     sources = obs.obs_enum_sources()
     for source in sources:
-        if source_name = obs.obs_source_get_name(source):
+        if source_name == obs.obs_source_get_name(source):
             rules_source = source
             break
     obs.source_list_release(sources)
 
-    if rules_source:
+    if ok and rules_source:
         # Get source data
         rules_data = obs.obs_source_get_settings(rules_source)
 
         # Get filter source
-        rules_filter_source = obs.obs_source_get_filter_by_name(rule_source, filter_name)
+        rules_filter_source = obs.obs_source_get_filter_by_name(rules_source, filter_name)
     else:
-        exit(f"Couldn't find source {source_name}")
+        print(f"Couldn't find source {source_name}")
+        ok = False
 
-    if rules_filter_source:
+    if ok and rules_filter_source:
         # Get filter data
         rules_filter_data = obs.obs_source_get_settings(rules_filter_source)
 
@@ -67,19 +75,21 @@ def reset_rules_variables:
         set_rules_text(rules_list[0])
         next_line = 1
     else:
-        exit(f"Couldn't find filter {filter_name}")
+        print(f"Couldn't find filter {filter_name}")
+        ok = False
 
     # Testing - check data
     speed_y = obs.obs_data_get_int(rules_filter_data, "speed_y")
     text_on = obs.obs_data_get_string(rules_data, "text")
-    print(f"{text}, {speed_y}")
-    exit(0)
+    print(f"{text_on}, {speed_y}")
+    ok = False
 
     # Set timer for scrolling effect
-    line_lens = sorted(set([len(x) for x in rules_list]))
-    max_len = line_lens[-1]
-    full_time = ((max_len * 2) * time_per_char) + display_time
-    obs.timer_add(rules_scrolling, full_time)
+    if ok:
+        line_lens = sorted(set([len(x) for x in rules_list]))
+        max_len = line_lens[-1]
+        full_time = ((max_len * 2) * time_per_char) + display_time
+        obs.timer_add(rules_scrolling, full_time)
 
 
 def set_rules_text(text):
