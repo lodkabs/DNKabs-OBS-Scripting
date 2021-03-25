3d659c129a758de6a3a68db2e80c0ae9fd35ab00
Kabs <70097297+lodkabs@users.noreply.github.com>
2021-03-25 20:46:10 +0000

    Managed to get window title to appear in text source

--- window_title.py
+++ window_title.py
@@ -9,10 +9,11 @@ GetWindowTextLength = ctypes.windll.user32.GetWindowTextLengthW
 IsWindowVisible = ctypes.windll.user32.IsWindowVisible
 
 # Global variables
-source_name    = ""
-title_name     = ""
-title_source   = None
-titles = []
+source_name     = ""
+selected_window = None
+title_name      = ""
+title_source    = None
+titles          = []
 
 def foreach_window(hwnd, lParam):
     global titles
@@ -21,7 +22,7 @@ def foreach_window(hwnd, lParam):
         length = GetWindowTextLength(hwnd)
         buff = ctypes.create_unicode_buffer(length + 1)
         GetWindowText(hwnd, buff, length + 1)
-        titles.append(buff.value)
+        titles.append([hwnd, buff.value])
     return True
 
 def get_source_from_source_name(name):
@@ -36,18 +37,6 @@ def get_source_from_source_name(name):
 
     return result_sourceitem
 
-def get_text_of_source(source):
-    ret_value = ""
-    source_data = obs.obs_source_get_settings(source)
-    data_json = obs.obs_data_get_json(source_data)
-    if data_json:
-        json_obj = json.loads(data_json)
-        if "text" in json_obj:
-            ret_value = json_obj["text"]
-    obs.obs_data_release(source_data)
-
-    return ret_value
-
 def set_text_of_source(source, text):
     source_data = obs.obs_source_get_settings(source)
     obs.obs_data_set_string(source_data, "text", text)
@@ -60,21 +49,23 @@ def populate_list_property_with_source_names(list_property):
 
     sources = obs.obs_enum_sources()
     for source in sources:
-        # Restrict selectable source list to Text objects
-        if get_text_of_source(source):
-            name = obs.obs_source_get_name(source)
-            obs.obs_property_list_add_string(list_property, name, name)
+        name = obs.obs_source_get_name(source)
+        obs.obs_property_list_add_string(list_property, name, name)
     obs.source_list_release(sources)
 
 def populate_list_property_with_window_titles(title_list):
     global titles
+
+    del titles[:]
 
     obs.obs_property_list_clear(title_list)
     obs.obs_property_list_add_string(title_list, "", "")
 
     EnumWindows(EnumWindowsProc(foreach_window), 0)
 
-    for title in set(titles):
+    titles_set = {t[1] for t in titles}
+
+    for title in titles_set:
         obs.obs_property_list_add_string(title_list, title, title)
 
 
@@ -86,31 +77,44 @@ def script_properties():
 
     list_property = obs.obs_properties_add_list(props, "source_name", "Source name", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
     populate_list_property_with_source_names(list_property)
-    # Button to refresh the drop-down list
-    obs.obs_properties_add_button(props, "source_button", "Refresh list of sources", lambda props, prop: True if populate_list_property_with_source_names(list_property) else True)
 
     title_list = obs.obs_properties_add_list(props, "title_name", "Window title", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
     populate_list_property_with_window_titles(title_list)
-    # Button to refresh the drop-down list
-    obs.obs_properties_add_button(props, "title_button", "Refresh list of windows", lambda props, prop: True if populate_list_property_with_window_titles(title_list) else True)
+
+    # Button to refresh the drop-down lists
+    obs.obs_properties_add_button(props, "button", "Refresh lists", lambda props, prop: True if populate_list_property_with_source_names(list_property) and populate_list_property_with_window_titles(title_list) else True)
 
     return props
 
 
 
-#def script_update(settings):
-#    global source_name, title_source
-#
-#    obs.timer_remove(set_title_text)
-#    source_name = obs.obs_data_get_string(settings, "source_name")
-#
-#    if source_name:
-#        title_source = get_source_from_source_name(source_name)
-#        obs.timer_add(set_title_text, 100)
+def script_update(settings):
+    global source_name, title_source, titles, title_name, selected_window
+
+    obs.timer_remove(set_title_text)
+    source_name = obs.obs_data_get_string(settings, "source_name")
+    title_name = obs.obs_data_get_string(settings, "title_name")
+
+    if source_name and title_name:
+        title_source = get_source_from_source_name(source_name)
+        set_text_of_source(title_source, title_name)
+
+        for t in titles:
+            if t[1] == title_name:
+                selected_window = t[0]
+        obs.timer_add(set_title_text, 100)
 
 
-def title_text():
-    global set_title_source, tempWindowName
+def set_title_text():
+    global title_source, title_name, selected_window
 
-    pass
+    length = GetWindowTextLength(selected_window)
+    buff = ctypes.create_unicode_buffer(length + 1)
+    GetWindowText(selected_window, buff, length + 1)
+
+    new_title = buff.value
+
+    if title_name != new_title:
+        title_name = new_title
+        set_text_of_source(title_source, title_name)
 
