0dc6f4a7673f3c77c30e97952bbd207907423a43
Kabs <70097297+lodkabs@users.noreply.github.com>
2021-04-17 18:30:57 +0100

    Many changes, should probably commit now

--- KabsBot/kabs_bot_reaction.py
+++ KabsBot/kabs_bot_reaction.py
@@ -11,29 +11,58 @@ def script_description():
               chat, commands, etc."""
 
 # Global variables
-source_name   = "Kabs_Bot"
-image_source  = None
-image_data    = None
+image_name   = "Kabs_Bot"
+image_source = None
+image_data   = None
+
+speech_name   = "Bot_Speak"
+speech_source = None
+speech_data   = None
+
 location      = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 image_path    = location + "/images/kabs_bot"
-response_file = location + "/kabs_bot_response.txt"
+response_file = location + "/speech_response.txt"
+
 
 def populate_variables():
-    global source_name, image_source, image_data
+    global image_name, image_source, image_data
+    global speech_name, speech_source, speech_data
 
+    obs.timer_remove(bot_speak)
+
+    if image_data:
+        obs.obs_data_release(image_data)
+
+    if speech_data:
+        obs.obs_data_release(specch_data)
+
+    ok1 = ok2 = False
     sources = obs.obs_enum_sources()
     for source in sources:
-        if obs.obs_source_get_name(source) == source_name:
+        if obs.obs_source_get_name(source) == image_name:
             image_source = source
+            ok1 = True
+        elif obs.obs_source_get_name(source) == speech_name:
+            speech_source = source
+            ok2 = True
+
+        if ok1 and ok2:
             break
+
     obs.source_list_release(sources)
 
     image_data = obs.obs_source_get_settings(image_source)
-
-    obs.obs_data_set_string(image_data, "file", f"{image_path}_lurk.gif")
+    obs.obs_data_set_string(image_data, "file", f"{image_path}_neutral.gif")
     obs.obs_source_update(image_source, image_data)
 
-    obs.obs_data_release(image_data)
+    speech_data = obs.obs_source_get_setting(speech_source)
+
+    obs.timer_add(bot_speak, 2000)
+
+
+def bot_speak():
+    pass
+
 
 
 def script_load(settings):
@@ -41,4 +70,10 @@ def script_load(settings):
 
 def script_update(settings):
     populate_variables()
+
+def script_unload(settings):
+    global image_data, speech_data
+
+    obs.obs_data_release(image_data)
+    obs.obs_data_release(speech_data)
 
